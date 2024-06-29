41c412594a860756fb8d0b0eacbad99b7c5f3cfb
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-06-29 15:12:42 +0800

    Add logic for playing a round

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const ROCK_C = 0,
     PAPER_C = 1,
     SCISSORS_C = 2;
 
+const COM_ID = 1,
+    HUMAN_ID = 2;
+
+let humanScore = 0,
+    computerScore = 0;
+
 function getComputerChoice() {
     let choiceN = Math.floor(Math.random() * 3),
         choiceText;
@@ -30,3 +36,53 @@ function getHumanChoice() {
 
     return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice)
+        console.log("You tied.");
+
+    const rockPlayer = humanChoice === "rock" ? HUMAN_ID
+        : computerChoice === "rock" ? COM_ID : 0;
+    const paperPlayer = humanChoice === "paper" ? HUMAN_ID
+        : computerChoice === "paper" ? COM_ID : 0;
+    const scissorsPlayer = humanChoice === "scissors" ? HUMAN_ID
+        : computerChoice === "scissors" ? COM_ID : 0;
+
+    if (rockPlayer) { // Only need 3C2 conditions = 3
+        if (paperPlayer) { // rock | paper
+            if (paperPlayer === COM_ID) {
+                    computerScore++;
+                    console.log("You lose. Rock loses to paper.");
+            } else {
+                humanScore++;
+                console.log("You win! Paper beats rock.");
+            }
+        } else if (scissorsPlayer) { // scissors | rock
+            if (rockPlayer === COM_ID) {
+                computerScore++;
+                console.log("You lose. Scissors loses to rock.");
+            } else {
+                humanScore++;
+                console.log("You win! Rock beats scissors.");
+            }
+        }
+    } else if (paperPlayer) { // can be just "else" but prioritizing readability here
+        if (scissorsPlayer) { // scissors | paper
+            if (scissorsPlayer === COM_ID) {
+                computerScore++;
+                console.log("You lose. Paper loses to scissors.");
+            } else {
+                humanScore++;
+                console.log("You win! Scissors beat paper.");
+            }
+        }
+    }
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+console.log(`SCORE human: ${humanScore} | computer: ${computerScore}`);
+console.log(`CHOICE human: ${humanChoice} | computer: ${computerChoice}`);
+
+playRound(humanChoice, computerChoice);
