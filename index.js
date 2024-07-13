21d182c2751ef9431109804763053157500ea609
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-07-13 15:13:06 +0800

    Refactor DOM update logic

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const ROCK_C = 0,
 
 const COM_ID = 1,
     HUMAN_ID = 2;
-    
-let humanScore = 0,
-    computerScore = 0;
 
 const WIN_CODE = 0,
     LOSE_CODE = 1,
@@ -16,7 +13,10 @@ const LOSE_COLOR = "red",
     WIN_COLOR = "green",
     TIE_COLOR = "blue";
 
-function makeResultsText(resultCode, extraText) {
+let humanScore = 0,
+    computerScore = 0;
+
+function getResultsText(resultCode, extraText) {
     const resultsPara = document.createElement("p");
     let resultText, resultColor;
 
@@ -73,16 +73,10 @@ function announceWinner(computerScore, humanScore) {
     }
 }
 
-function playGame() {
-
-
-    announceWinner(computerScore, humanScore);
-}
-
-function playRound(humanChoice, computerChoice) {
+function getResultsElement(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         const extraText = `You both chose ${humanChoice}.`;
-        return makeResultsText(TIE_CODE, extraText);
+        return getResultsText(TIE_CODE, extraText);
     };
 
     const rockPlayer = humanChoice === "rock" ? HUMAN_ID
@@ -96,28 +90,28 @@ function playRound(humanChoice, computerChoice) {
         if (paperPlayer) { // rock | paper
             if (paperPlayer === COM_ID) {
                 computerScore++;
-                return makeResultsText(LOSE_CODE,"Rock loses to paper.");
+                return getResultsText(LOSE_CODE, "Rock loses to paper.");
             } else {
                 humanScore++;
-                return makeResultsText(WIN_CODE,"Paper beats rock.");
+                return getResultsText(WIN_CODE, "Paper beats rock.");
             }
         } else if (scissorsPlayer) { // scissors | rock
             if (rockPlayer === COM_ID) {
                 computerScore++;
-                return makeResultsText(LOSE_CODE,"Scissors loses to rock.");
+                return getResultsText(LOSE_CODE, "Scissors loses to rock.");
             } else {
                 humanScore++;
-                return makeResultsText(WIN_CODE,"Rock beats scissors.");
+                return getResultsText(WIN_CODE, "Rock beats scissors.");
             }
         }
     } else if (paperPlayer) { // can be just "else" but prioritizing readability here
         if (scissorsPlayer) { // scissors | paper
             if (scissorsPlayer === COM_ID) {
                 computerScore++;
-                return makeResultsText(LOSE_CODE,"Paper loses to scissors.");
+                return getResultsText(LOSE_CODE, "Paper loses to scissors.");
             } else {
                 humanScore++;
-                return makeResultsText(WIN_CODE,"Scissors beat paper.");
+                return getResultsText(WIN_CODE, "Scissors beat paper.");
             }
         }
     }
@@ -129,25 +123,34 @@ const comScoreCounter = document.querySelector("#com-score .counter");
 const winnerBox = document.querySelector("#winner");
 const thingButtons = document.querySelectorAll("button");
 
-thingButtons.forEach(b => 
-    b.addEventListener("click", e => {
-        const humanChoice = e.target.id;
-        const computerChoice = getRandomChoice();
-        const resultPara = playRound(humanChoice, computerChoice);
-        console.log(resultPara);
-        resultBox.replaceChild(resultPara, resultBox.firstElementChild);
-        humanScoreCounter.textContent = humanScore;
-        comScoreCounter.textContent = computerScore;
-        if (humanScore === 5 || computerScore === 5) {
-            const winnerAnnouncement = announceWinner(computerScore, humanScore);
-            winnerBox.textContent = winnerAnnouncement;
+function updateWinnerAnnouncement() {
+    if (humanScore === 5 || computerScore === 5) {
+        const announcementText = announceWinner(computerScore, humanScore);
+        winnerBox.textContent = announcementText;
+        // One game is played until one scores upto 5 then another game is played
+        computerScore = 0;
+        humanScore = 0;
+    } else {
+        winnerBox.textContent = "";
+    }
+}
 
-            // One game is played until one scores upto 5 then another game is played
-            computerScore = 0;
-            humanScore = 0;
-        } else {
-            winnerBox.textContent = "";
-        }
-    })
-)
+function updateCounters() {
+    humanScoreCounter.textContent = humanScore;
+    comScoreCounter.textContent = computerScore;
+}
 
+function updateResults(e) {
+    const humanChoice = e.target.id;
+    const computerChoice = getRandomChoice();
+    const resultPara = getResultsElement(humanChoice, computerChoice);
+    resultBox.replaceChild(resultPara, resultBox.firstElementChild);
+}
+
+function playRound(e) {
+    updateResults(e);
+    updateCounters();
+    updateWinnerAnnouncement();
+};
+
+thingButtons.forEach(b => b.addEventListener("click", playRound));
